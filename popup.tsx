c8b58b3444423e3d1779bfbebce61c6e1ba78aeb
Dibyajyoti Ganguly <gangulyd49@gmail.com>
2026-05-04 09:27:32 +0530

    Scaffold haptic extension using plasmo

--- popup.tsx
+++ popup.tsx
@@ -1,25 +1,10 @@
-import { useState } from "react"
+import "./style.css"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
-
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
-    </div>
+    <main className="w-80 p-4">
+      <h1 className="text-xl font-semibold">Hapticos</h1>
+    </main>
   )
 }
 
